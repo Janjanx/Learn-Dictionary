a1f4bbdc8b0ecedd32a4411d11facaaeceeb4b5b
Janjan <bubble-gumz@hotmail.com>
2017-10-14 21:46:55 +0700

    UI Success

---
+++ LearnDictionary/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  LearnDictionary
+//
+//  Created by Janjan on 10/14/2560 BE.
+//  Copyright © 2560 Janjanx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var usernameTextField: UILabel!
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    @IBAction func loginTextField(_ sender: Any) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
